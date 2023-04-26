ce8b6d5fd84a351716bd9ab139570fbdb9de2092
nairem <130434943+nai-rem@users.noreply.github.com>
2023-04-26 12:37:56 -0700

    Chapter 1 Challenge 1 completed

---
+++ chapter_1/candybarsales.cpp
@@ -0,0 +1,20 @@
+//Programming challenge. Chapter 1 - Challenge 1.
+//Calculate how much an originization earns selling candy bars.
+#include <iostream>
+using namespace std;
+
+int main(){
+    double candybars_sold,candybar_price,total_earned;
+
+    cout << "How many candy bars were sold? " << endl;
+    cin >> candybars_sold;
+
+    cout << "How much does a candy bar cost?" << endl; 
+    cin >> candybar_price;
+
+    total_earned = candybar_price * candybars_sold;
+
+    cout << "The total amount earned is $" << total_earned << endl;
+
+    return 0;
+}
