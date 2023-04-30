9440f32e502acb7e0513ae35af623f38145d3419
nairem <130434943+nai-rem@users.noreply.github.com>
2023-04-30 06:43:59 -0700

    Fixed a typo in a comment

--- chapter_2/challenge_1.cpp
+++ chapter_2/challenge_1.cpp
@@ -1,12 +1,13 @@
 /*
-C++ Early Objects 10th Edition
-Chapter 2, Challenge 2: Sum of Two Numbers
+C++ Early Objects 10th Edition.
+Chapter 2, Challenge 1: Sum of Two Numbers.
 
 "Write a program that stores the interger 50 and 100 in variables and stores
 the sum of these two numberes in a variable named total. Display the total on the screen."
 
 4/30/2023
 */
+
 #include <iostream>
 using namespace std;
 
@@ -18,4 +19,5 @@ int main(){
 
     total = number1 + number2;
     cout << number1 << " plus " << number2 << " equals " << total << endl;
+    return 0;
 }
